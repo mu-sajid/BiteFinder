60418fe6a0207423e8bc7bea229e7b39543efbf9
Will <109585548+will-ixs@users.noreply.github.com>
2024-11-19 21:50:06 -0600

    location backend (wip)

--- backend/app.py
+++ backend/app.py
@@ -18,9 +18,38 @@ scorer = VSMScorer(data_parser.idfs, title_weight=0.1, body_weight=0.9)
 def recommend():
     data = request.json
     query = data.get('query', '')
+    
     #location_lat = data.get('lat', '')
     #location_lng = data.get('lng', '')
-    #send Nearby Request with latitude and longitude
+    #range = data.get('rng', '')
+    search_range = 50
+    location_lat = 37.7937
+    location_lng = -122.3965
+    data = {
+        "includedTypes": ["restaurant"],
+        "maxResultCount": 100,
+        "locationRestriction": {
+            "circle": {
+                "center": {
+                    "latitude": location_lat,
+                    "longitude": location_lng
+                },
+                "radius": search_range
+            }
+        }
+    }
+    endpoint = 'https://places.googleapis.com/v1/places:searchNearby'
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': ,
+        'X-Goog-FieldMask': 'places.displayName.text, places.types, places.editorialSummary.text, places.regularOpeningHours.openNow, places.priceLevel'
+    }
+    response = requests.post(endpoint, headers=headers, json=data)
+    
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({'error': 'Failed to retrieve data', 'status_code': response.status_code}), response.status_code
     #construct documents out of those place, pass them through scorer as done below
 
     # Calculate recommendations based on query
